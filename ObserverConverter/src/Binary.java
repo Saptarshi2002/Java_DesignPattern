eca3e3003ca3e9f665c4d1fe7394ac29d81b9d38
unknown <saptarshim44@gmail.com>
2025-04-07 12:33:10 +0530

    Observer Converter Added

--- ObserverConverter/src/Binary.java
+++ ObserverConverter/src/Binary.java
@@ -7,13 +7,12 @@ public class Binary implements Observer {
 
 	@Override
 	public void update(int n, boolean showBin, boolean showOct, boolean showHex) {
-		// TODO Auto-generated method stub
 		if (showBin) {
 //			frame = new JFrame("Binary Value");
             frame.setSize(300, 100);
 //            JLabel label = new JLabel("Binary: " + Integer.toBinaryString(n));
 //            label.setText("Binary: " + Integer.toBinaryString(n));
-            label.setText("Binary: " + Integer.toBinaryString(n));
+            label.setText("Binary:  " + Integer.toBinaryString(n));
             frame.add(label);
             frame.setLocationRelativeTo(null);
             frame.setVisible(true);
